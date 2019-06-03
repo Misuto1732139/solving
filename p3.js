2eeb86f5a7565c4fc3d5ec9a696a9bf23136d7ff
Misuto1732139 <49706264+Misuto1732139@users.noreply.github.com>
2019-06-03 16:21:55 +0900

    Update p3.js

--- p3.js
+++ p3.js
@@ -1,23 +1,27 @@
-const EPS = 0.0001; // constは定数の定義
+const EPS = 0.0001;    // 許容誤差
 
 function main() {
-    var a = 0.0, b = 1.0;  // 初期値
+    console.log("x^3 + x - 1 のニュートン法による数値計算");
 
-    console.log("x^3 + x - 1 の2分法による数値計算");
+    var a = 1.0;
     console.log("初期値 a=" + a);
-    console.log("初期値 b=" + b);
-    var x = nibun(a, b);  // 解
-    console.log("近似解 x = " + x);
-}
+
+    var b;
+
+    // ここにニュートン法による演算の主要部分を記述する
 
 function nibun(a, b) {
     var c;
-
+    var output=document.querySelector("#output");
     do {
         c = (a + b) / 2.0;  // 2分計算
-        console.log(c);
+        let str=document.createTextNode(c);
+        let p =document.createElement("p");
+        p.appendChild(str);
+        output.appendChild(p);
         if ((func_y(c) * func_y(a)) < 0) b = c;  // 式(1.2)
         else a = c;  // 式(1.3)
     } while (Math.abs(a - b) > EPS);  // 収束判別　式(1.4)の変形
     return c;
+    console.log("近似解 x = " + b);
 }
